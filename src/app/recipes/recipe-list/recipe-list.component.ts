0af2dc503d9e80be5fc18fdb853616b4839d4a4b
Maczelka Norman <norman.maczelka@ge.com>
2020-02-21 16:26:04 +0100

    Initial Shopping list project.

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe("Test Recipe", "Recipe description", "https://pinchofyum.com/wp-content/uploads/Lo-Mein-Recipe.jpg"),
+    new Recipe("Test Recipe2", "Recipe description2", "https://pinchofyum.com/wp-content/uploads/Lo-Mein-Recipe.jpg")
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
