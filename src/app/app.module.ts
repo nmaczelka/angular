90124dd8b7a743efcb07f11cf2ddd80a4f92a242
Maczelka Norman <norman.maczelka@ge.com>
2020-03-06 15:04:34 +0100

    Add own directive.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
 import { WarningAlertComponenet } from './warning-alert/warning-alert-component';
+import { BasicHighLightDirective } from './basic-highlight/basic-highlight.directive';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { WarningAlertComponenet } from './warning-alert/warning-alert-component'
     ServerComponent,
     ServersComponent,
     WarningAlertComponenet,
-    SuccessAlertComponent
+    SuccessAlertComponent,
+    BasicHighLightDirective
   ],
   imports: [
     BrowserModule,
