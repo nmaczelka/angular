fd72bf6989dd19a37ef6c582c67795713451bcbb
Maczelka Norman <norman.maczelka@ge.com>
2020-04-02 13:24:39 +0200

    Add dropdown directive.

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -4,7 +4,7 @@ import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
-  styleUrls: ['./recipe-detail.component.css']
+  styleUrls: ['./recipe-detail.component.css','./../../app.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
